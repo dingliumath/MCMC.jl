2c7145c20e1481fdf4d7f12421a167d699a38814
fredo <frederic.testard@gmail.com>
2013-09-11 19:27:53 +0200

    still porting diffs, parsing errors remaining

--- src/autodiff/test.jl
+++ src/autodiff/test.jl
@@ -2,91 +2,47 @@ cd("p:/Documents/julia/MCMC.jl/src/autodiff")
 cd("~/devl/MCMC.jl/src/autodiff")
 pwd()
 
-include("/home/fredo/devl/MCMC.jl/src/autodiff/mymod.jl")
+@windows_only include("p:/Documents/julia/MCMC.jl/src/autodiff/mymod.jl")
+@unix_only include("/home/fredo/devl/MCMC.jl/src/autodiff/mymod.jl")
 
 ######
-	y = 12
+
+
+
+	b = 12
+	Abcd.debug(:( a+b ), a=0)
+	b = [1, 2, 3]
+	Abcd.debug(:( a+b ), a=0)
+
 	Abcd.generateModelFunction(:( x+y ), gradient=true, x=0, debug=true)
 	f, a,b,c = Abcd.generateModelFunction(:( x+y ), gradient=true, x=0)
 	f, a,b,c = Abcd.generateModelFunction(:( a=x ), gradient=true, x=0)
 	f([-13.])
 
-
-	y = 1.
-	ex = :( x' )
-
-	m = Abcd.ParsingStruct()
-	Abcd.setInit!(m, [(:x,[0. 1 ; 1 2])])
-	# dump(m)
-	Abcd.parseModel!(m, ex)
-	# m.source
-	Abcd.unfold!(m)
-	# m.exprs
-	Abcd.uniqueVars!(m)
-	Abcd.categorizeVars!(m)
-
-	Abcd.preCalculate(m)
-	Abcd.backwardSweep!(m)
-
-	Abcd.vhint
-	import Abcd.LLAcc
-	for (k,v) in Abcd.vhint
-		t = typeof(v)
-		println("$k, $t, $(t==LLAcc ? "-" : size(v))")
-	end
-	m.exprs
-	m.dexprs
-
-	f, a,b,c = Abcd.generateModelFunction(:( x+y ), gradient=true, x=[0. 1 ; 1 2])
-	f([1.,0,0,0])
-	Abcd.generateModelFunction(:( x+y ), gradient=true, x=[0. 1 ; 1 2], debug=true)
-
-	Abcd.eval(:( (symbol("##830"))([1.,2]) ) )
-
-###############
-
-	Abcd.generateModelFunction(ex, gradient=false, debug=true, mu=0.0)
-	Abcd.generateModelFunction(ex, gradient=true, debug=true, mu=0.0, sd=1.0)
-	Abcd.generateModelFunction(ex, gradient=true, debug=true, mu=0.0)
-
-
-	f, a,b,c = Abcd.generateModelFunction(ex, gradient=true, mu=0.0, sd=1.0)
-
-	f([0.,1])
-
-	include("mymod.jl")
-
-
-	tex = quote
-		a = sin(nb)
-		x = sum(a)
-		a[2] = 4+a[3]
-		sum(a)
-	end
-
-	Abcd.generateModelFunction(tex, gradient=true, debug=true, nb=4.)
-
-	m = Abcd.ParsingStruct()
-	Abcd.setInit!(m, [(:nb,4.)])
-	Abcd.parseModel!(m, tex)
-	Abcd.unfold!(m)
-	# m.exprs
-	Abcd.uniqueVars!(m)
-	Abcd.categorizeVars!(m)
-	m.varsset
-	m.accanc
-	Abcd.preCalculate(m)
-
-	Abcd.backwardSweep!(m)
-
-	m.exprs
-	m.dexprs
-
-
-##### 
-
-	using NumericExtensions
-
-	evaluate{T<:FloatingPoint}(::Myfunc, x::T) = sum(x ./ p - (n-x) ./ (1 - p)) * ds
-
 ################
+
+# simulate dataset
+	srand(1)
+	nbeta = 10 # number of predictors, including intercept
+	beta0 = randn((nbeta,))
+
+	n = 1000
+	X = [ones(n) randn((n, nbeta-1))]
+	Y = float64( rand(n) .< ( 1 ./ (1. + exp(X * beta0))) )
+
+# define model
+ex = quote
+	vars ~ Normal(0, 1.0)  # Normal prior, std 1.0 for predictors
+	prob = 1 / (1. + exp(X * vars)) 
+	Y ~ Bernoulli(prob)
+end
+
+Abcd.debug(ex, vars=zeros(nbeta))  
+Abcd.generateModelFunction(ex, vars=zeros(nbeta), gradient=true, debug=true)  
+
+ex2 = quote
+	vars ~ Normal(0, 1.0)  # Normal prior, std 1.0 for predictors
+	prob = sum(vars) * sum(X)
+	prob ~ Normal(1,2)
+end
+Abcd.debug(ex2, vars=0.0)  # no method Bernoulli(Array{Float64,1},)
